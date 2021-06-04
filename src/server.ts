22579539a8a25377f35ead125233dc3ad7a891f8
Jose A. Pennarroche <jose.penarroche@globant.com>
2021-06-04 11:32:05 -0300

    chore: preparing server for tests use

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import {middleware} from '@lib';
 import {json, urlencoded} from 'body-parser';
 import {IContext} from 'main';
 
-const register = (app: Express, ctx: IContext): Express => {
+const register = (ctx: IContext, app: Express): Express => {
   // parse application/x-www-form-urlencoded
   app.use(urlencoded({extended: false}));
   // parse application/json
@@ -18,18 +18,22 @@ const register = (app: Express, ctx: IContext): Express => {
   app.use(middleware.morgan);
   // register global error handler
   app.use(middleware.error);
+
   return app;
 };
 
 export const run = (ctx: IContext) => {
   const {port, host} = common;
-  const app = express();
 
   const localPort = Number(port || 3000);
   const localhost = host ?? 'localhost';
 
+  const app = register(ctx, express());
+
   //Register all applications and launch server
-  register(app, ctx).listen(localPort, localhost, () => {
+  app.listen(localPort, localhost, () => {
     console.log(`Listening through port: ${localPort}`);
   });
+
+  return app;
 };
